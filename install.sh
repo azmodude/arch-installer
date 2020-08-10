a66b81388915df63dcc5f0d87832958ffc0f0ebb
Gordon Schulz <gordon.schulz@gmail.com>
2020-08-10 09:28:30 +0200

    Add comment

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# quick set all options for testing purposes in vagrant
 sudo INSTALL_DISK=/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi3-0-4 ROOT_PASSWORD=test LUKS_PASSPHRASE=test HOSTNAME_FQDN=test.azmo.ninja OS_SIZE=5 SWAP_SIZE=1 /vagrant/arch_install_zfs.sh
