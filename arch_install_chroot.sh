4bad1c8a28a37fdf309b2c3d8fb83e82715d168f
Gordon Schulz <gordon.schulz@gmail.com>
2020-09-22 18:10:47 +0200

    Formatting updates

--- arch_install_chroot.sh
+++ arch_install_chroot.sh
@@ -12,8 +12,8 @@ ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 echo "${green}Generating locale${reset}"
 locale-gen
 echo "${green}Configuring hostname${reset}"
-echo "${HOSTNAME_FQDN}" > /etc/hostname
-cat > /etc/hosts << END
+echo "${HOSTNAME_FQDN}" >/etc/hostname
+cat >/etc/hosts <<END
 127.0.0.1   localhost.localdomain localhost
 127.0.1.1   ${HOSTNAME_FQDN} ${HOSTNAME%%.*}
 END
@@ -35,7 +35,7 @@ mkdir -p /etc/zfs/zfs-list.cache
 touch /etc/zfs/zfs-list.cache/dpool
 ln -s /usr/lib/zfs/zed.d/history_event-zfs-list-cacher.sh /etc/zfs/zed.d
 # shellcheck disable=SC2015
-zed && sleep 5 && zfs set canmount=on dpool && zfs set canmount=off dpool && \
+zed && sleep 5 && zfs set canmount=on dpool && zfs set canmount=off dpool &&
     pkill zed || true
 
 systemctl enable zfs-import-cache
@@ -51,7 +51,7 @@ systemctl enable zfs.target
 systemctl enable home-root.mount
 
 echo "${green}Generating mkinitcpio.conf${reset}"
-cat > /etc/mkinitcpio.conf << END
+cat >/etc/mkinitcpio.conf <<END
 MODULES=(${MODULES})
 BINARIES=()
 FILES=()
@@ -67,7 +67,7 @@ sed -r -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*$/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/" /e
 sed -r -i "s/GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX=\"rd.luks.name=${LUKS_PARTITION_UUID_OS}=crypt-system rd.luks.options=discard ${FSPOINTS//\//\\/} consoleblank=120 apparmor=1 lsm=lockdown,yama,apparmor rw\"/" /etc/default/grub
 
 case "${IS_EFI}" in
-    (true)  grub-install --target=x86_64-efi --efi-directory=/boot/esp --bootloader-id=GRUB --recheck;;
-    (false) grub-install --target=i386-pc --recheck "${INSTALL_DISK}";;
+true) grub-install --target=x86_64-efi --efi-directory=/boot/esp --bootloader-id=GRUB --recheck ;;
+false) grub-install --target=i386-pc --recheck "${INSTALL_DISK}" ;;
 esac
 grub-mkconfig -o /boot/grub/grub.cfg
