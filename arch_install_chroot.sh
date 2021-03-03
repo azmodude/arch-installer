4cd8c1cfdfd5dfeb1b405edbeb585ecea29d965b
Gordon Schulz <gordon@gordonschulz.de>
2021-03-03 11:48:27 +0100

    Disable grub submenu

--- arch_install_chroot.sh
+++ arch_install_chroot.sh
@@ -59,6 +59,7 @@ sed -r -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*$/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/" /e
 sed -r -i "s/GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX=\"rd.luks.name=${LUKS_PARTITION_UUID_OS}=crypt-system rd.luks.options=discard ${FSPOINTS//\//\\/} consoleblank=120 apparmor=1 lsm=lockdown,yama,apparmor rw\"/" /etc/default/grub
 sed -r -i "s/^GRUB_DEFAULT=.*$/GRUB_DEFAULT=saved/" /etc/default/grub
 sed -r -i "s/^#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/" /etc/default/grub
+sed -r -i "s/^#GRUB_DISABLE_SUBMENU/GRUB_DISABLE_SUBMENU=y/" /etc/default/grub
 
 case "${IS_EFI}" in
 true) grub-install --target=x86_64-efi --efi-directory=/boot/esp --bootloader-id=GRUB --recheck ;;
