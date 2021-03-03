473c9c96fe09d98f3f1d625ea3154b833dd6447b
Gordon Schulz <gordon@gordonschulz.de>
2021-03-03 09:06:09 +0100

    Change to Ubuntu keyserver

--- arch_install_chroot.sh
+++ arch_install_chroot.sh
@@ -19,7 +19,7 @@ cat >/etc/hosts <<END
 END
 
 # get and lsign archzfs keys
-pacman-key --keyserver pool.sks-keyservers.net -r DDF7DB817396A49B2A2723F7403BD972F75D9D76
+pacman-key --keyserver keyserver.ubuntu.com -r DDF7DB817396A49B2A2723F7403BD972F75D9D76
 pacman-key --lsign-key DDF7DB817396A49B2A2723F7403BD972F75D9D76
 
 echo "${green}Enabling AppArmor${reset}"
