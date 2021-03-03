077d2aff6da92b12c6a75392e7f5aa6e43a7c2d3
Gordon Schulz <gordon@gordonschulz.de>
2021-03-03 09:34:52 +0100

    Fix sed expression

--- arch_install_chroot.sh
+++ arch_install_chroot.sh
@@ -63,7 +63,7 @@ echo "root:${ROOT_PASSWORD}" | chpasswd
 echo "${green}Installing bootloader${reset}"
 sed -r -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*$/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/" /etc/default/grub
 sed -r -i "s/GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX=\"rd.luks.name=${LUKS_PARTITION_UUID_OS}=crypt-system rd.luks.options=discard ${FSPOINTS//\//\\/} consoleblank=120 apparmor=1 lsm=lockdown,yama,apparmor rw\"/" /etc/default/grub
-sed -r -i "s/^GRUB_DEFAULT=.*$/GRUB_DEFAULT=saved"
+sed -r -i "s/^GRUB_DEFAULT=.*$/GRUB_DEFAULT=saved/"
 sed -r -i "s/^#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/"
 
 case "${IS_EFI}" in
