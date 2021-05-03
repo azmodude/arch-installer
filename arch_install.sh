ff3a2810f39be810c8aaed90fe1bcb20159a2302
Gordon Schulz <gordon@gordonschulz.de>
2021-05-03 14:48:36 +0200

    We don't need to *install* the packages, we need the repos

--- arch_install.sh
+++ arch_install.sh
@@ -111,7 +111,7 @@ preinstall() {
     # install needed stuff for install
     echo "${green}Installing necessary packages${reset}"
     pacman -Sy --needed --noconfirm parted util-linux dialog bc dosfstools \
-        arch-install-scripts xfsprogs lvm2 zfs-linux zfs-utils gptfdisk openssl
+        arch-install-scripts xfsprogs lvm2 gptfdisk openssl
     # set keys to German
     loadkeys de
     # enable NTP
