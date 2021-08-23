5f30b09bc6923f18f2e34c7d46ab6fdff67b7204
Gordon Schulz <gordon@gordonschulz.de>
2021-08-23 09:12:37 +0200

    Add VM Creation Script

---
+++ create_vm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ISO="/var/lib/libvirt/images/archlinux-2021.08.23-x86_64-azmo-zfs.iso"
+IMAGE="/var/lib/libvirt/images/archtest.qcow2"
+
+set -vx
+sudo virsh --connect qemu:///system destroy archtest
+sudo virsh --connect qemu:///system undefine --domain archtest --remove-all-storage
+sudo virt-install --name=archtest --vcpus=4 \
+    --boot loader=//usr/share/OVMF/OVMF_CODE.fd \
+    --memory=2048 --cdrom=${ISO} --disk \
+    ${IMAGE},size=5,bus=sata --os-variant=archlinux
