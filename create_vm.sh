6f84bb3c1b3ea8d3b2f2cb2c29a8c1ee00719191
Gordon Schulz <gordon@gordonschulz.de>
2021-08-31 17:52:52 +0200

    Update create_vm support script

--- create_vm.sh
+++ create_vm.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-ISO="/var/lib/libvirt/images/archlinux-2021.08.23-x86_64-azmo-zfs.iso"
+ISO="/var/lib/libvirt/images/archlinux-2021.08.31-x86_64-azmo-zfs.iso"
 IMAGE="/var/lib/libvirt/images/archtest.qcow2"
 
-set -vx
 sudo virsh --connect qemu:///system destroy archtest
-sudo virsh --connect qemu:///system undefine --domain archtest --remove-all-storage
-sudo virt-install --name=archtest --vcpus=4 \
-    --boot loader=//usr/share/OVMF/OVMF_CODE.fd \
-    --memory=2048 --cdrom=${ISO} --disk \
-    ${IMAGE},size=40,bus=sata --os-variant=archlinux
+
+if [[ "$1" == "install" ]]; then
+    set -vx
+    sudo virsh --connect qemu:///system undefine --domain archtest --remove-all-storage
+    sudo virt-install --name=archtest --vcpus=4 \
+        --boot loader=//usr/share/OVMF/OVMF_CODE.fd \
+        --memory=2048 --cdrom=${ISO} --disk \
+        ${IMAGE},size=40,bus=sata --os-variant=archlinux
+else
+    set -vx
+    sudo virsh --connect qemu:///system undefine --domain archtest
+    sudo virt-install --name=archtest --vcpus=4 \
+        --boot loader=//usr/share/OVMF/OVMF_CODE.fd \
+        --memory=2048 --disk ${IMAGE},bus=sata --os-variant=archlinux
+fi
