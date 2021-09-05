6a5cc660cb54f7724c090694f8f845b46265b218
Gordon Schulz <gordon@gordonschulz.de>
2021-09-05 18:49:29 +0200

    LVM_SIZE -> OS_SIZE

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 # quick set all options for testing purposes in vagrant
 sudo INSTALL_DISK=/dev/disk/by-id/ata-QEMU_HARDDISK_QM00001 \
-    ROOT_PASSWORD=test LUKS_PASSPHRASE=test HOSTNAME_FQDN=test.azmo.ninja LVM_SIZE=20 \
+    ROOT_PASSWORD=test LUKS_PASSPHRASE=test HOSTNAME_FQDN=test.azmo.ninja OS_SIZE=20 \
     SWAP_SIZE=4 ./arch_install.sh
