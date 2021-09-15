4d746e028d11c84de67e8161029e959541841f08
Gordon Schulz <gordon@gordonschulz.de>
2021-09-15 09:27:04 +0200

    Parametrize key

--- arch_zfs_install.sh
+++ arch_zfs_install.sh
@@ -2,9 +2,11 @@
 
 set -Eeuxo pipefail
 
+ARCHZFS_KEY="DDF7DB817396A49B2A2723F7403BD972F75D9D76"
+
 # get and lsign archzfs keys
-pacman-key --keyserver keyserver.ubuntu.com -r DDF7DB817396A49B2A2723F7403BD972F75D9D76
-pacman-key --lsign-key DDF7DB817396A49B2A2723F7403BD972F75D9D76
+pacman-key --keyserver keyserver.ubuntu.com -r ${ARCHZFS_KEY}
+pacman-key --lsign-key ${ARCHZFS_KEY}
 
 pacman -Sy archzfs-linux archzfs-linux-lts archzfs-linux-zen
 
