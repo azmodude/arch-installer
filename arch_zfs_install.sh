5131aca168044d7b3e40a71d71c056ed600ef67c
Gordon Schulz <gordon@gordonschulz.de>
2021-09-15 09:26:25 +0200

    Add flags

--- arch_zfs_install.sh
+++ arch_zfs_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -Eeuxo pipefail
+
 # get and lsign archzfs keys
 pacman-key --keyserver keyserver.ubuntu.com -r DDF7DB817396A49B2A2723F7403BD972F75D9D76
 pacman-key --lsign-key DDF7DB817396A49B2A2723F7403BD972F75D9D76
