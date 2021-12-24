7559569bfa0729ccbe0d70e9c8f04fddcba6738b
Gordon Schulz <gordon@gordonschulz.de>
2021-12-24 12:42:46 +0100

    Update archzfs install

--- zfs/arch_zfs_install.sh
+++ zfs/arch_zfs_install.sh
@@ -4,15 +4,39 @@ set -Eeuxo pipefail
 
 ARCHZFS_KEY="DDF7DB817396A49B2A2723F7403BD972F75D9D76"
 
-cat <<-'EOF' >> /etc/pacman.conf
-[archzfs]
-    Server = http://archzfs.com/$repo/x86_64
-    Server = http://mirror.sum7.eu/archlinux/archzfs/$repo/$arch
-    Server = https://mirror.biocrafting.net/archlinux/archzfs/$repo/$arch
-    Server = https://mirror.in.themindsmaze.com/archzfs/$repo/$arch
-[zfs-linux]
-    Server = http://kernels.archzfs.com/$repo
+[[ ! -e /etc/pacman.d/archzfs ]] && \
+	cat <<-'EOF' >> /etc/pacman.d/archzfs
+Server = http://archzfs.com/$repo/x86_64
+Server = http://mirror.sum7.eu/archlinux/archzfs/$repo/x86_64
+Server = https://mirror.biocrafting.net/archlinux/archzfs/$repo/x86_64
 EOF
+
+[[ ! -e /etc/pacman.d/archzfs-kernels ]] && echo 'Server = http://kernels.archzfs.com/$repo' > /etc/pacman.d/archzfs-kernels
+
+if ! grep -q /etc/pacman.conf archzfs; then
+	cat <<-'EOF' >> /etc/pacman.conf
+[archzfs]
+Include = /etc/pacman.d/archzfs
+EOF
+fi
+if ! grep -q /etc/pacman.conf zfs-linux; then
+	cat <<-'EOF' >> /etc/pacman.conf
+[zfs-linux]
+Include = /etc/pacman.d/archzfs-kernels
+EOF
+fi
+if ! grep -q /etc/pacman.conf zfs-linux-lts; then
+	cat <<-'EOF' >> /etc/pacman.conf
+[zfs-linux-lts]
+Include = /etc/pacman.d/archzfs-kernels
+EOF
+fi
+if ! grep -q /etc/pacman.conf zfs-linux-zen; then
+	cat <<-'EOF' >> /etc/pacman.conf
+[zfs-linux-zen]
+Include = /etc/pacman.d/archzfs-kernels
+EOF
+fi
 
 # get and lsign archzfs keys
 pacman-key --keyserver keyserver.ubuntu.com -r ${ARCHZFS_KEY}
